e3c2d955e9361872640772de2ae8acb57bc157e9
JV <jaiveer.chadda@gmail.com>
2026-06-01 00:04:07 +0400

    Started doing a little bit of cleanup in the main line function

--- source/line.zsh
+++ source/line.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
@@ -11,15 +11,18 @@ line() {
   local -i 10 line_len=$COLUMNS
   local line_chr='─'
 
-  # if there are no non-flag inputs, print the defaults and get out of the way
-  if [[ -z "$@" ]] { echo -n "${(pr:$line_len::$line_chr:)}$end"; return; }
+  # if there are no non-flag inputs, print the defaults and exit
+  if ! (( $# )) {
+    echo -nE "${(pr:line_len::$line_chr:)}$end"
+    return
+  }
 
-  # if $1 is a number
+  # if $1 is an integer
   if [[ "$1" == <-> ]] {
     line_len=$1                 # set $1 as the line length,
     line_chr="${2:-$line_chr}"  # and $2 as the line char
 
-  # if $2 is a number
+  # if $2 is an integer
   } elif [[ "$2" == <-> ]] {
     line_len=$2                 # set $2 as the line length,
     line_chr="${1:-$line_chr}"  # and $1 as the line char
@@ -29,5 +32,5 @@ line() {
 
   }  # if both are empty, use the defaults
 
-  echo -n "${(pr:$line_len::$line_chr:)}$end"
+  echo -nE "${(pr:line_len::$line_chr:)}$end"
 }
