4c74e1c45134041998c531da7b2f381b93976001
JV <jaiveer.chadda@gmail.com>
2026-06-01 00:22:58 +0400

    Updated the glob checks to allow decimal digits too

--- source/line.zsh
+++ source/line.zsh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
 
-# ——————————————————————————————————————————————————————————————————————————— #
-
 line() {
+  setopt local_options warn_create_global
 
   local end=$'\n'
   if [[ "$1" == '-n' ]] { end= ; shift; }
@@ -17,18 +16,18 @@ line() {
     return
   }
 
-  # if $1 is an integer
-  if [[ "$1" == <-> ]] {
-    line_len=$1                 # set $1 as the line length,
-    line_chr="${2:-$line_chr}"  # and $2 as the line char
+  # if `$1` is a number
+  if [[ "$1" == (<->(.(<->|)|)|.<->) ]] {  # (\d+(.\d*)?|.\d+)
+    line_len=$1                 # set `$1` as the line length,
+    line_chr="${2:-$line_chr}"  # and `$2` as the line char
 
-  # if $2 is an integer
-  } elif [[ "$2" == <-> ]] {
-    line_len=$2                 # set $2 as the line length,
-    line_chr="${1:-$line_chr}"  # and $1 as the line char
+  # if `$2` is a number
+  } elif [[ "$2" == (<->(.(<->|)|)|.<->) ]] {
+    line_len=$2                 # set `$2` as the line length,
+    line_chr="${1:-$line_chr}"  # and `$1` as the line char
 
-  } elif [[ -n "$1" ]] { line_chr="$1" # if $1 has a value, set it as the char
-  } elif [[ -n "$2" ]] { line_chr="$2" # if $2 has a value, set it as the char
+  } elif [[ "$1" ]] { line_chr="$1" # if `$1` has a value, set it as the char
+  } elif [[ "$2" ]] { line_chr="$2" # if `$2` has a value, set it as the char
 
   }  # if both are empty, use the defaults
 
